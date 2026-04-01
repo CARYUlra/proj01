776fb5ec4908e2c97a77e8e935f5417e57f48764
Your Name <CARYUlra>
2026-04-01 08:32:43 -0300

    banana

---
+++ ex01/script.js
@@ -0,0 +1,8 @@
+
+let numero = Number(prompt("digite um número:"));
+
+if (numero >= 0) {
+    alert("saldo positivo");
+} else{
+    alert("saldo negativo")
+}
---
+++ ex02/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("digite sua idade"));
+
+if (numero >= 18) {
+    alert("você é maior de idade");
+} else{
+    alert("você é menor de idade")
+}
